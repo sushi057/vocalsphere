274648758a6b3b3772fb3198dd020f62bc923376
sushi <champsuvash@gmail.com>
2023-08-13 20:56:07 +0545

    audio file added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div className=" flex flex-row">
+      <div className="flex flex-row p-2">
         <VoiceBox />
         {/* <TextBox /> */}
       </div>
--- src/components/VoiceBox.js
+++ src/components/VoiceBox.js
@@ -1,30 +1,35 @@
 import React from "react";
+import AudioPlayer from "react-audio-player";
 import mic from "../assets/images/microphone.png";
 import folder from "../assets/images/folder.png";
+import audiofile from "../assets/audio/harvard.wav";
 
 function VoiceBox() {
+  function play() {
+    new Audio(audiofile).play();
+  }
   return (
-    <div className="flex-col">
-      <div className="border-4 border-teal-400 rounded-lg p-2">
-        <div>
-          <h2 className="text-xl font-bold">English</h2>
-          <div className="flex flex-row">
-            <img
-              src={mic}
-              alt="microphone"
-              className="object-cover h-12 w-12"
-            />
-            <img
-              src={folder}
-              alt="folder icon"
-              className="object-cover h-12 w-12"
-            />
-          </div>
+    <div className="w-1/2 h-96 border-4 border-teal-400 rounded-lg p-3">
+      <div>
+        <h2 className="text-2xl font-bold">English</h2>
+        <div className="flex flex-row justify-center items-center gap-24 h-48 ">
+          <img
+            src={mic}
+            alt="microphone icon"
+            className="object-cover h-16 w-16"
+          />
+          <img
+            src={folder}
+            alt="folder icon"
+            className="object-cover h-16 w-16"
+          />
         </div>
+      </div>
 
+      <div>
+        <h2 className="text-2xl font-bold">Nepali</h2>
         <div>
-          <h2 className="text-xl font-bold">Nepali</h2>
-          <div>{/* An audio output file ready to be played */}</div>
+          <AudioPlayer src={audiofile} />
         </div>
       </div>
     </div>
