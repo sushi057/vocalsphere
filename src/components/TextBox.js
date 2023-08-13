45dd31ed60d6de5fdb48f855b6f36c3c758f6c7a
sushi <champsuvash@gmail.com>
2023-08-14 01:03:05 +0545

    added audio player

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -2,21 +2,21 @@ import React from "react";
 
 function TextBox() {
   return (
-    <div className="flex flex-col w-1/2 border-4 gap-4 border-teal-400 rounded-lg p-3">
+    <div className="flex flex-col w-1/2 border-4 gap-4 border-teal-400 rounded-lg px-6 py-4">
       <div className="mb-8">
         <div className="text-2xl font-bold">English Text</div>
         <textarea
-          rows="6"
+          rows="5"
           cols="50"
-          className="w-full h-full border-4 rounded-lg"
+          className="w-full h-full border-4 rounded-lg p-3 mt-2"
         ></textarea>
       </div>
       <div>
         <div className="text-2xl font-bold">Nepali Text</div>
         <textarea
-          rows="6"
+          rows="5"
           cols="50"
-          className="w-full h-full border-4 rounded-lg"
+          className="w-full h-full border-4 rounded-lg p-3 mt-2"
         ></textarea>
       </div>
     </div>
--- src/components/VoiceBox.js
+++ src/components/VoiceBox.js
@@ -1,39 +1,46 @@
 import React from "react";
-import AudioPlayer from "react-audio-player";
 import mic from "../assets/images/microphone.png";
 import folder from "../assets/images/folder.png";
 import audiofile from "../assets/audio/harvard.wav";
+import AudioPlayer from "react-audio-player";
 
 function VoiceBox() {
   return (
-    <div className="w-1/2 h-max border-4 border-teal-400 rounded-lg p-3">
+    <div className="w-1/2 h-max border-4 border-teal-400 rounded-lg px-6 py-4">
       <div>
         <h2 className="text-2xl font-bold">English</h2>
-        <div className="flex flex-auto flex-row justify-center items-center gap-40 h-48">
-          <img
-            src={mic}
-            alt="microphone icon"
-            className="object-cover h-16 w-16"
-          />
-          <img
-            src={folder}
-            alt="folder icon"
-            className="object-cover h-16 w-16"
-          />
+        <p>Input your audio file.</p>
+        <div className="flex flex-auto flex-row justify-center items-center gap-40 h-40">
+          <div className="flex flex-col items-center">
+            <img
+              src={mic}
+              alt="microphone icon"
+              className="object-cover h-16 w-16"
+            />
+            <button type="button" className="border-2 rounded-lg px-3 mt-2">
+              Record
+            </button>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src={folder}
+              alt="folder icon"
+              className="object-cover h-16 w-16"
+            />
+            <button type="button" className="border-2 rounded-lg px-3 mt-2">
+              Upload{" "}
+            </button>
+          </div>
         </div>
-        <div className="border-2 border-cyan-500 text-center rounded-lg">
-          This section will have the input audio file.
+        <div className="flex justify-center">
+          <AudioPlayer src={audiofile} controls />
         </div>
       </div>
 
       <div className="mt-12 h-48">
         <h2 className="text-2xl font-bold">Nepali</h2>
-        <div className="flex flex-row justify-center mt-4 border-2 border-cyan-500 rounded-lg">
-          <p>
-            This section will have the
-            <span className="text-bold"> Output </span>
-            audio file.
-          </p>
+        <div className="flex justify-center">
+          <AudioPlayer src={audiofile} controls />
         </div>
       </div>
     </div>
